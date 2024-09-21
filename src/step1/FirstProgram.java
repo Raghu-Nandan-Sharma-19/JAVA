aa78ff1e5f3be712882af70fdea10e8a2ddd8cf3
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2024-09-21 07:42:52 +0530

    Modified first program

--- src/step1/FirstProgram.java
+++ src/step1/FirstProgram.java
@@ -1,6 +1,6 @@
 package step1;
 
-public class FirstProgram {  // this is a block
+public class FirstProgram {  // this is a block of code
     public static void main(String[] args) {
         System.out.println("Hello World using Java");
     }
