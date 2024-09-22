d550523e71b542150d5ae5f4a35ba7efe75e60a8
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2024-09-22 19:08:45 +0530

    Variables and Data types

---
+++ src/step1/VariablesAndDataTypes.java
@@ -0,0 +1,29 @@
+package step1;
+
+public class VariablesAndDataTypes {
+    public static void main(String[] args) {
+        boolean isPassed = true;
+        System.out.println(isPassed);
+
+        byte marks = 126;
+        System.out.println(marks);
+
+        short shortCount = 1000;
+        System.out.println(shortCount);
+
+        int count = 100000;
+        System.out.println(count);
+
+        long longCount = 1000000000;
+        System.out.println(longCount);
+
+        float pi = 3.13f;
+        System.out.println(pi);
+
+        double secondPi = 3.14321214242d;
+        System.out.println(secondPi);
+
+        char ch = 'a';
+        System.out.println(ch);
+    }
+}
