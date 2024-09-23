4770ae768ca305c2390a938fb1cc1aa2e6778e06
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2024-09-23 23:12:34 +0530

    Math class methods

---
+++ src/step1/MathClassMethods.java
@@ -0,0 +1,26 @@
+package step1;
+
+public class MathClassMethods {
+    public static void main(String[] args) {
+        int x = 4;
+        int y = 2;
+
+        System.out.println(Math.min(4, 2));
+
+        System.out.println(Math.max(4, 2));
+
+        System.out.println(Math.sqrt(x));
+
+        System.out.println(Math.pow(x, y));
+
+        System.out.println(Math.abs(y));
+
+        System.out.println((int) (Math.random() * 6 + 1));
+
+        System.out.println(Math.floor(5.66778));
+
+        System.out.println(Math.ceil(5.66778));
+
+        System.out.println(Math.round(5.666224));
+    }
+}
