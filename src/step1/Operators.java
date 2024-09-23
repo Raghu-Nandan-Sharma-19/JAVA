cf75549a5eb0511897764e43aaae5317635e8c77
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2024-09-23 06:19:55 +0530

    Operators

---
+++ src/step1/Operators.java
@@ -0,0 +1,65 @@
+package step1;
+
+public class Operators {
+    public static void main(String[] args) {
+
+//        // Arithmetic Operators
+//        int a = 12;
+//        int b = 7;
+//
+//        // Addition
+//        System.out.println(a + b);
+//
+//        // Subtraction
+//        System.out.println(a - b);
+//
+//        // multiplication
+//        System.out.println(a * b);
+//
+//        // division
+//        System.out.println((double) a / b);
+//
+//        // modulo operation --> remainder after division
+//        System.out.println(a % b);
+
+
+//        // relational operators
+//        int c = 12;
+//        int d = 10;
+//
+//        System.out.println(c == d);
+//
+//        System.out.println(c != d);
+//
+//        System.out.println(c > d);
+//
+//        System.out.println(c < d);
+//
+//        System.out.println(c >= d);
+//
+//        System.out.println(c <= d);
+
+//        // Logical operator
+//        boolean exp1 = true;
+//        boolean exp2 = false;
+//
+//        System.out.println(exp1 && exp2);
+//
+//        System.out.println(exp1 || exp2);
+//
+//        System.out.println(!exp1);
+
+        // Other operators
+        int a = 10;
+
+        a++;  // a = a + 1
+        System.out.println(a);
+        ++a;
+        System.out.println(a);
+
+        a--;  // a = a - 1
+        System.out.println(a);
+        --a;
+        System.out.println(a);
+    }
+}
