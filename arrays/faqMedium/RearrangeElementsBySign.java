c6a00745b3c41ce306b3d71c97259d62d7d8e461
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2025-02-02 18:27:24 +0530

    Added comments in Rearrange elements by sign

--- arrays/faqMedium/RearrangeElementsBySign.java
+++ arrays/faqMedium/RearrangeElementsBySign.java
@@ -6,20 +6,29 @@ public class RearrangeElementsBySign {
     private static class Solution {
         public int[] rearrangeArray(int[] nums) {
             int n = nums.length;
+            // index of positive and negative elements
             int positiveIndex = 0;
             int negativeIndex = 1;
+            // ans array to store rearranged elements
             int[] ans = new int[n];
 
+            // iterating through the array
             for (int i = 0; i < n; i++) {
+                // if current element is positive
                 if (nums[i] > 0) {
+                    // add current element to ans[positiveIndex]
                     ans[positiveIndex] = nums[i];
+                    // increment positive index to next positive index
                     positiveIndex += 2;
                 } else {
+                    // add current element to ans[negativeIndex]
                     ans[negativeIndex] = nums[i];
+                    // increment negative index to next negative index
                     negativeIndex += 2;
                 }
             }
 
+            // return the ans array
             return ans;
         }
     }
