7d400cc6359038f42314673725d653a307ae636f
Raghu-Nandan-Sharma-19 <raghunandansharma1905@gmail.com>
2025-02-20 22:19:21 +0530

    4 sum optimal solution

--- arrays/faqMedium/FourSum.java
+++ arrays/faqMedium/FourSum.java
@@ -2,39 +2,56 @@ package arrays.faqMedium;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Scanner;
-import java.util.Set;
 
 public class FourSum {
     private static class Solution {
         public List<List<Integer>> fourSum(int[] nums, int target) {
-            List<List<Integer>> ans = new ArrayList<>();
-            Set<List<Integer>> set = new HashSet<>();
+            List<List<Integer>> ans = new ArrayList<>(); // List to store the result
             int n = nums.length;
+            Arrays.sort(nums); // Sort the array to make it easier to avoid duplicates
 
             for (int i = 0; i < n; i++) {
+                if (i > 0 && nums[i] == nums[i - 1]) {
+                    continue; // Skip duplicate elements
+                }
+
                 for (int j = i + 1; j < n; j++) {
-                    Set<Integer> hashSet = new HashSet<>();
-                    for (int k = j + 1; k < n; k++) {
-                        int sum = nums[i] + nums[j] + nums[k];
-                        int fourth = target - sum;
+                    if (j > i + 1 && nums[j] == nums[j - 1]) {
+                        continue; // Skip duplicate elements
+                    }
 
-                        if (hashSet.contains(fourth)) {
-                            List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k], fourth);
+                    int k = j + 1;
+                    int l = n - 1;
 
-                            Collections.sort(temp);
-                            set.add(temp);
+                    while (k < l) {
+                        long sum = nums[i];
+                        sum += nums[j];
+                        sum += nums[k];
+                        sum += nums[l];
+
+                        if (sum == target) {
+                            List<Integer> temp = Arrays.asList(nums[i], nums[j], nums[k], nums[l]);
+                            ans.add(temp);
+                            k++;
+                            l--;
+
+                            if (k < l && nums[k] == nums[k - 1]) {
+                                k++;
+                            }
+
+                            if (k < l && nums[l] == nums[l + 1]) {
+                                l--;
+                            }
+                        } else if (sum < target) {
+                            k++;
+                        } else {
+                            l--;
                         }
-
-                        hashSet.add(nums[k]);
                     }
                 }
             }
-            
-            ans.addAll(set);
 
             return ans;
         }
