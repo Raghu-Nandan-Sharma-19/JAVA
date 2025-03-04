df5daae0941146d2937d833337ba9723768566ec
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-03-04 23:55:56 +0530

    Majority element 2 Optimal approach using moore's voting algorithm

--- arrays/faqHard/MajorityElement2.java
+++ arrays/faqHard/MajorityElement2.java
@@ -8,27 +8,52 @@ public class MajorityElement2 {
     private static class Solution {
         public List<Integer> majorityElementTwo(int[] nums) {
             int n = nums.length;
-            List<Integer> result = new ArrayList<>();
+            int cnt1 = 0, cnt2 = 0;
+            int element1 = Integer.MIN_VALUE, element2 = Integer.MIN_VALUE;
 
+            // First pass: Find potential candidates for majority elements
             for (int i = 0; i < n; i++) {
-                if (result.size() == 0 || result.get(0) != nums[i]) {
-                    int count = 0;
-                    for (int j = 0; j < n; j++) {
-                        if (nums[j] == nums[i]) {
-                            count++;
-                        }
-                    }
-
-                    if (count > (n / 3)) {
-                        result.add(nums[i]);
-                    }
-                }
-
-                if (result.size() == 2) {
-                    break;
+                if (cnt1 == 0 && nums[i] != element2) {
+                    cnt1 = 1;
+                    element1 = nums[i];
+                } else if (cnt2 == 0 && nums[i] != element1) {
+                    cnt2 = 1;
+                    element2 = nums[i];
+                } else if (element1 == nums[i]) {
+                    cnt1++;
+                } else if (element2 == nums[i]) {
+                    cnt2++;
+                } else {
+                    cnt1--;
+                    cnt2--;
                 }
             }
 
+            List<Integer> result = new ArrayList<>();
+            cnt1 = 0;
+            cnt2 = 0;
+
+            // Second pass: Verify the candidates
+            for (int i = 0; i < n; i++) {
+                if (element1 == nums[i]) {
+                    cnt1++;
+                }
+
+                if (element2 == nums[i]) {
+                    cnt2++;
+                }
+            }
+
+            // Check if the candidates appear more than n/3 times
+            if (cnt1 > (n / 3)) {
+                result.add(element1);
+            }
+
+            if (cnt2 > (n / 3)) {
+                result.add(element2);
+            }
+
+            // return result
             return result;
         }
     }
@@ -38,6 +63,7 @@ public class MajorityElement2 {
         int n = scanner.nextInt();
         int[] nums = new int[n];
 
+        // Read input array
         for (int i = 0; i < n; i++) {
             nums[i] = scanner.nextInt();
         }
