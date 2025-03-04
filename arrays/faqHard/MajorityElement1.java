555d0cc0b062674d199ea56bc53e70d9a2afdec1
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-03-04 23:13:44 +0530

    Majority element 1 better approach

--- arrays/faqHard/MajorityElement1.java
+++ arrays/faqHard/MajorityElement1.java
@@ -1,22 +1,22 @@
 package arrays.faqHard;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class MajorityElement1 {
     private static class Solution {
         public int majorityElement(int[] nums) {
             int n = nums.length;
-            
-            for (int i = 0; i < n; i++) {
-                int count = 1;
-                for (int j = i + 1; j < n; j++) {
-                    if (nums[j] == nums[i]) {
-                        count++;
-                    }
-                }
+            HashMap<Integer, Integer> mpp = new HashMap<>();
 
-                if (count > (n / 2)) {
-                    return nums[i];
+            for (int num : nums) {
+                mpp.put(num, mpp.getOrDefault(num, 0) + 1);
+            }
+
+            for (Map.Entry<Integer, Integer> it : mpp.entrySet()) {
+                if (it.getValue() > (n / 2)) {
+                    return it.getKey();
                 }
             }
 
