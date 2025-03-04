fd9203b88dae42a93688c787e4fae0885e6cdfdf
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-03-05 00:34:56 +0530

    Find the missing and repeatig number better solution

--- arrays/faqHard/FindTheMissingAndRepeatingNumber.java
+++ arrays/faqHard/FindTheMissingAndRepeatingNumber.java
@@ -8,19 +8,21 @@ public class FindTheMissingAndRepeatingNumber {
             int n = nums.length;
             int missing = -1;
             int repeating = -1;
+            int[] hash = new int[n + 1];
+
+            for (int i = 0; i < n; i++) {
+                hash[nums[i]]++;
+            }
 
             for (int i = 1; i <= n; i++) {
-                int count = 0;
-                for (int j = 0; j < n; j++) {
-                    if (nums[j] == i) {
-                        count++;
-                    }
+                if (hash[i] == 0) {
+                    missing = i;
+                } else if (hash[i] == 2) {
+                    repeating = i;
                 }
 
-                if (count == 0) {
-                    missing = i;
-                } else if (count == 2) {
-                    repeating = i;
+                if (repeating != -1 && missing != -1) {
+                    break;
                 }
             }
 
