ce2eca94d2a15c587b6adee1a6d4b70de955eab4
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-03-05 02:34:50 +0530

    Merge two sorted arrays without extra space optimal solution 1

--- arrays/faqHard/MergeTwoSortedArraysWithoutExtraSpace.java
+++ arrays/faqHard/MergeTwoSortedArraysWithoutExtraSpace.java
@@ -1,33 +1,40 @@
 package arrays.faqHard;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class MergeTwoSortedArraysWithoutExtraSpace {
     private static class Solution {
-        public void merge(int[] nums1, int m, int[] nums2, int n) {
-            int[] merged = new int[n + m];
-            int left = 0;
-            int right = 0;
-            int index = 0;
+        // Helper method to swap elements between two arrays
+        private static void swap(int[] nums1, int[] nums2, int idx1, int idx2) {
+            int temp = nums1[idx1];
+            nums1[idx1] = nums2[idx2];
+            nums2[idx2] = temp;
+        }
 
-            while (left < m && right < n) {
-                if (nums1[left] <= nums2[right]) {
-                    merged[index++] = nums1[left++];
+        // Method to merge two sorted arrays without extra space
+        public void merge(int[] nums1, int m, int[] nums2, int n) {
+            int left = m - 1;
+            int right = 0;
+
+            // Swap elements to ensure nums1 contains the smallest elements
+            while (left >= 0 && right < n) {
+                if (nums1[left] > nums2[right]) {
+                    swap(nums1, nums2, left, right);
+                    left--;
+                    right++;
                 } else {
-                    merged[index++] = nums2[right++];
+                    break;
                 }
             }
 
-            while (left < m) {
-                merged[index++] = nums1[left++];
-            }
+            // Sort both arrays
+            Arrays.sort(nums1, 0, m);
+            Arrays.sort(nums2); 
 
-            while (right < n) {
-                merged[index++] = nums2[right++];
-            }
-
-            for (int i = 0; i < (m + n); i++) {
-                nums1[i] = merged[i];
+            // Copy elements from nums2 to nums1
+            for (int i = m; i < m + n; i++) {
+                nums1[i] = nums2[i - m];
             }
         }
     }
@@ -43,20 +50,18 @@ public class MergeTwoSortedArraysWithoutExtraSpace {
             nums1[i] = scanner.nextInt();
         }
 
-      
         int[] nums2 = new int[n];
         for (int i = 0; i < n; i++) {
             nums2[i] = scanner.nextInt();
         }
 
         Solution solution = new Solution();
-
         solution.merge(nums1, m, nums2, n);
 
         for (int i = 0; i < (m + n); i++) {
             System.out.print(nums1[i] + " ");
         }
-        
+
         scanner.close();
     }
 }
