8f0a89cbd3b5eb555b66aeaa6cd82ca99b34fc94
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-03-05 02:00:10 +0530

    Maximum product subarray in array brute force

---
+++ arrays/faqHard/MaximumProductSubarrayInArray.java
@@ -0,0 +1,39 @@
+package arrays.faqHard;
+
+import java.util.Scanner;
+
+public class MaximumProductSubarrayInArray {
+    private static class Solution {
+        public int maxProduct(int[] nums) {
+            int n = nums.length;
+            int maxProd = Integer.MIN_VALUE;
+
+            for (int i = 0; i < n; i++) {
+                for (int j = i; j < n; j++) {
+                    int prod = 1;
+                    for (int k = i; k <= j; k++) {
+                        prod *= nums[k];
+                    }
+                    maxProd = Math.max(maxProd, prod);
+                }
+            }
+
+            return maxProd;
+        }
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        Solution solution = new Solution();
+        System.out.println(solution.maxProduct(nums));
+
+        scanner.close();
+    }
+}
