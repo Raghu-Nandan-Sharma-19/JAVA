52098c2d602990366daa2176f94c6d55a5f196f0
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-04-01 05:56:03 +0530

    Longest consecutive sequence in array

--- hashing/faqs/LongestConsecutiveSequenceInArray.java
+++ hashing/faqs/LongestConsecutiveSequenceInArray.java
@@ -1,45 +1,28 @@
 package hashing.faqs;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class LongestConsecutiveSequenceInArray {
-    // linear search method to find the next greater element
-    private static boolean linearSearch(int[] nums, int x) {
-        for (int num : nums) {
-            if (num == x) {
-                return true;
-            }
-        }
-
-        return false;
-    }
-    
     // method to find longest consecutive sequence in array
     private static int longestConsecutive(int[] nums) {
-        // size of the array
-        int n = nums.length;
-        // taking longest as 0
-        int longest = 0;
+        Arrays.sort(nums);
+        int longest = 1;
+        int cnt = 0;
+        int lastSmaller = Integer.MIN_VALUE;
 
-        // iterating through the array
-        for (int i = 0; i < n; i++) {
-            // taking the first consecutive element
-            int x = nums[i];
-            // assigning cnt as 1 for 1st consecutive element
-            int cnt = 1;
-
-            // linear search to find next consecutive element 
-            while (linearSearch(nums, x + 1)) {
-                // increasing x and cnt by 1
-                x++;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] - 1 == lastSmaller) {
                 cnt++;
+                lastSmaller = nums[i];
+            } else if (nums[i] != lastSmaller) {
+                cnt = 1;
+                lastSmaller = nums[i];
             }
-            
-            // updating longest with the max of cnt and longest
-            longest = Math.max(cnt, longest);
+
+            longest = Math.max(longest, cnt);
         }
 
-        // returning the longest consecutive sequence
         return longest;
     }
 
