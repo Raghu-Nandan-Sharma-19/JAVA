edf92588b72c2388e8606dc45df5b5f47d9ad20b
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-04-03 15:04:06 +0530

    Longest subarray with sum k brute force approach

---
+++ hashing/faqs/LongestSubarrayWithSumK.java
@@ -0,0 +1,42 @@
+package hashing.faqs;
+
+import java.util.Scanner;
+
+public class LongestSubarrayWithSumK {
+    private static int longestSubarray(int[] nums, int k) {
+        int n = nums.length;
+        int longest = 0;
+
+        for (int i = 0; i  < n; i++) {
+            for (int j = i; j < n; j++) {
+                int sum = 0;
+                
+                for (int l = i; l <= j; l++) {
+                    sum += nums[l];
+                }
+
+                if (sum == k) {
+                    longest = Math.max(longest, j - i + 1);
+                }
+            }   
+        }
+
+        return longest;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }   
+
+        int k = scanner.nextInt();
+
+        System.out.println(longestSubarray(nums, k));
+
+        scanner.close();
+    }
+}
