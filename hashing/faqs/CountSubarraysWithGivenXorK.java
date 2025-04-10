71b567dfc11ec32ee8d64cef5e45f3ec85134cdc
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-04-10 07:49:35 +0530

    Count subarrays with given xor k optimal solution

--- hashing/faqs/CountSubarraysWithGivenXorK.java
+++ hashing/faqs/CountSubarraysWithGivenXorK.java
@@ -1,22 +1,38 @@
 package hashing.faqs;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class CountSubarraysWithGivenXorK {
     private static int subarraysWithXorK(int[] nums, int k) {
+        // Length of the array
         int n = nums.length;
+        
+        // Variable to keep track of the running XOR
+        int xr = 0;
+        
+        // Map to store frequency of XOR values seen so far
+        Map<Integer, Integer> map = new HashMap<>();
+        
+        // Initialize with 0 XOR having count 1
+        map.put(xr, map.getOrDefault(xr, 0) + 1);
+        
+        // Counter for subarrays with XOR equal to k
         int cnt = 0;
 
         for (int i = 0; i < n; i++) {
-            int xorr = 0;
-            
-            for (int j = i; j < n; j++) {
-                xorr ^= nums[j];
+            // Update running XOR with current element
+            xr ^= nums[i];
 
-                if (xorr == k) {
-                    cnt++;
-                }
-            }
+            // Calculate XOR value needed to achieve target k
+            int x = xr ^ k;
+            
+            // Add count of subarrays ending at current position with XOR k
+            cnt += map.getOrDefault(x, 0);
+            
+            // Update frequency of current running XOR
+            map.put(xr, map.getOrDefault(xr, 0) + 1);
         }
 
         return cnt;
