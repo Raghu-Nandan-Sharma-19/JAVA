6f426258ed8907b5ea623c921e384879ece63480
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-04-10 07:24:51 +0530

    Count subarrays with given xor k brute force approach

---
+++ hashing/faqs/CountSubarraysWithGivenXorK.java
@@ -0,0 +1,42 @@
+package hashing.faqs;
+
+import java.util.Scanner;
+
+public class CountSubarraysWithGivenXorK {
+    private static int subarraysWithXorK(int[] nums, int k) {
+        int n = nums.length;
+        int cnt = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int xorr = 0;
+                
+                for (int K = i; K <= j; K++) {
+                    xorr ^= nums[K];
+                }
+
+                if (xorr == k) {
+                    cnt++;
+                }
+            }
+        }
+
+        return cnt;
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int[] nums = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = scanner.nextInt();
+        }
+
+        int k = scanner.nextInt();
+
+        System.out.println(subarraysWithXorK(nums, k));
+
+        scanner.close();
+    }
+}
