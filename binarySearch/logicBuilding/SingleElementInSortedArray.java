3066c11698569c34ec0d07a93286303a7273a5d3
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-04-18 14:04:06 +0530

    Single element in sorted array brute force 2 using xor

--- binarySearch/logicBuilding/SingleElementInSortedArray.java
+++ binarySearch/logicBuilding/SingleElementInSortedArray.java
@@ -5,28 +5,13 @@ import java.util.Scanner;
 public class SingleElementInSortedArray {
     private static int singleNonDuplicate(int[] nums) {
         int n = nums.length;
+        int ans = 0;
 
-        if (n == 1) {
-            return nums[0];
-        }
-        
         for (int i = 0; i < n; i++) {
-            if (i == 0) {
-                if (nums[i] != nums[i + 1]) {
-                    return nums[0];
-                }
-            } else if (i == n - 1) {
-                if (nums[i - 1] != nums[i]) {
-                    return nums[n - 1];
-                }
-            } else {
-                if (nums[i - 1] != nums[i] && nums[i] != nums[i + 1]) {
-                    return nums[i];
-                }
-            }
+            ans ^= nums[i];
         }
 
-        return -1;
+        return ans;
     }
     
     public static void main(String[] args) {
