83cf6250ae34cc56b9caa473ebd1c7b598dcba9b
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-05-28 19:39:56 +0530

    Find nth root of a number optimal using binary search

--- binarySearch/onAnswers/FindNthRootOfANumber.java
+++ binarySearch/onAnswers/FindNthRootOfANumber.java
@@ -4,16 +4,31 @@ import java.util.Scanner;
 
 public class FindNthRootOfANumber {
     private static int nthRoot(int N, int M) {
-        for (int i = 1; i <= M; i++) {
-            long val = (long) (Math.pow(i, N));
+        // Initialize the search range
+        int low = 1;
+        int high = M;
 
+        // Perform binary search to find the Nth root of M
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            // Calculate mid raised to the power N
+            long val = (long) (Math.pow(mid, N));
+
+            // If mid^N equals M, mid is the Nth root
             if (val == M) {
-                return i;
-            } else if (val > M) {
-                break;
+                return mid;
+            }
+
+            // If mid^N is less than M, search in the higher half
+            if (val < M) {
+                low = mid + 1;
+            } else {
+                // If mid^N is greater than M, search in the lower half
+                high = mid - 1;
             }
         }
 
+        // If no integer Nth root exists, return -1
         return -1;
     }
 
