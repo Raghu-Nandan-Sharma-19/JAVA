d6d5b84cc59094d74490b841d47122b49a1e1bf7
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-05-28 18:50:51 +0530

    Find Square Root of a Number Optimal using Binary Search

--- binarySearch/onAnswers/FindSquareRootOfANumber.java
+++ binarySearch/onAnswers/FindSquareRootOfANumber.java
@@ -4,19 +4,27 @@ import java.util.Scanner;
 
 public class FindSquareRootOfANumber {
     private static long floorSqrt(long n) {
-        long ans = 1;
-        
-        for (long i = 2; i <= n; i++) {
-            long val = i * i;
+        // bianry search search space
+        long low = 1;
+        long high = n;
+
+        // Perform binary search to find the floor of the square root
+        while (low <= high) {
+            // Prevents overflow
+            long mid = low + (high - low) / 2; 
+            long val = mid * mid;
 
             if (val <= n) {
-                ans = i;
+                // mid*mid is less than or equal to n, so move right to find a larger answer
+                low = mid + 1;
             } else {
-                break;
+                // mid*mid is greater than n, so move left
+                high = mid - 1;
             }
         }
 
-        return ans;
+        // high will be the floor of the square root of n
+        return high;
     }
     
     public static void main(String[] args) {
