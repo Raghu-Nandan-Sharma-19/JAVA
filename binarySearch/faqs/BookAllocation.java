1716f3da9ab01bcea35b715440942e72e0c0c730
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-06-01 16:59:16 +0530

    Book Allocation Optimal using Binary Search

--- binarySearch/faqs/BookAllocation.java
+++ binarySearch/faqs/BookAllocation.java
@@ -3,6 +3,7 @@ package binarySearch.faqs;
 import java.util.Scanner;
 
 public class BookAllocation {
+    // Method to find max element in array
     private static int maxElement(int[] nums, int n) {
         int maxi = Integer.MIN_VALUE;
 
@@ -13,6 +14,7 @@ public class BookAllocation {
         return maxi;
     }
 
+    // method to find sum of elements of array
     private static int sum(int[] nums, int n) {
         int sum = 0;
 
@@ -24,37 +26,58 @@ public class BookAllocation {
     }
 
     private static int countStudents(int[] nums, int n, int pages) {
+        // Initialize the number of students required to 1
         int student = 1;
+
+        // Initialize the current number of pages allocated to a student
         int pagesStudent = 0;
 
+        // Iterate through each book
         for (int i = 0; i < n; i++) {
+            // If adding the current book does not exceed the page limit
             if ((pagesStudent + nums[i]) <= pages) {
-                pagesStudent += nums[i];
+                // Allocate book to current student
+                pagesStudent += nums[i]; 
             } else {
+                // Allocate books to next student
                 student++;
+
+                // Start counting pages for new student
                 pagesStudent = nums[i];
             }
         }
 
+        // Return the total number of students required
         return student;
     }
 
     private static int findPages(int[] nums, int m) {
         int n = nums.length;
 
+        // If there are more students than books, allocation is not possible
         if (m > n) return -1;
 
+        /* The minimum possible value is the maximum element
+           (no student can get less than the largest book) */
         int low = maxElement(nums, n);
+        /* The maximum possible value is the sum of all pages 
+           (one student gets all books) */
         int high = sum(nums, n);
 
-        for (int pages = low; pages <= high; pages++) {
-            int countStudent = countStudents(nums, n, pages);
+        // Binary search to find the minimum possible maximum pages
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
 
-            if (countStudent == m) {
-                return pages;
+            // If more students are needed, increase the minimum possible value
+            if (countStudents(nums, n, mid) > m) {
+                low = mid + 1;
+            } else {
+                // Otherwise, try for a better (smaller) maximum
+                high = mid - 1;
             }
         }
 
+        // The lowest value for which allocation is possible
         return low;
     }
 
