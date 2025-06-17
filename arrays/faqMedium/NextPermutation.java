7dcf228901be6ba651a6839bdfc0d883d6fda6c2
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-06-17 17:29:44 +0530

    Next Permutation Optimal Solution

--- arrays/faqMedium/NextPermutation.java
+++ arrays/faqMedium/NextPermutation.java
@@ -1,8 +1,5 @@
 package arrays.faqMedium;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
 public class NextPermutation {
@@ -12,73 +9,51 @@ public class NextPermutation {
         nums[idx2] = temp;
     }
 
-    private static boolean match(int[] nums, List<Integer> list) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] != list.get(i)) return false;
+    private static void reverse(int[] nums, int idx1, int idx2) {
+        while (idx1 < idx2) {
+            swap(nums, idx1, idx2);
+            idx1++;
+            idx2--;
         }
-        return true;
-    }
-
-    private static void helperFunction(int ind, int[] nums, List<List<Integer>> ans) {
-        if (ind == nums.length) {
-            List<Integer> temp = new ArrayList<>();
-            for (int num : nums) temp.add(num);
-            ans.add(temp);
-            return;
-        }
-
-        for (int i = ind; i < nums.length; i++) {
-            swap(nums, ind, i);
-            helperFunction(ind + 1, nums, ans);
-            swap(nums, ind, i);
-        }
-
-        return;
-    }
-
-    private static List<List<Integer>> getAllPermutations(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        helperFunction(0, nums, ans);
-        
-        Collections.sort(ans, (a, b) -> {
-            for (int i = 0; i < a.size(); i++) {
-                if (!a.get(i).equals(b.get(i))) {
-                    return a.get(i) - b.get(i);
-                }
-            }
-
-            return 0;
-        });
-
-        return ans;
     }
 
     private static void nextPermutation(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>(getAllPermutations(nums));
+        int n = nums.length;
+
         int idx = -1;
 
-        for (int i = 0; i < ans.size(); i++) {
-            if (match(nums, ans.get(i))) {
+        // Find the first index from the end where nums[i] < nums[i + 1]
+        for (int i = n - 2; i >= 0; i--) {
+            if (nums[i] < nums[i + 1]) {
                 idx = i;
                 break;
             }
         }
 
-        int nextPermutationIndex = -1;
-
-        if (idx == ans.size() - 1) nextPermutationIndex = 0;
-        else nextPermutationIndex = idx + 1;
-
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = ans.get(nextPermutationIndex).get(i);
+        /*
+         * If no such index is found, the array is in descending order.
+         * Reverse the whole array to get the smallest permutation.
+         */
+        if (idx == -1) {
+            reverse(nums, 0, n - 1);
+            return;
         }
 
-        return;
+        // Find the first index from the end where nums[i] > nums[idx] and swap
+        for (int i = n - 1; i >= idx; i--) {
+            if (nums[i] > nums[idx]) {
+                swap(nums, i, idx);
+                break;
+            }
+        }
+        
+        // Reverse the subarray to the right of idx to get the next permutation
+        reverse(nums, idx + 1, n - 1);
     }
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int n = 3;
+        int n = scanner.nextInt();
         int[] nums = new int[n];
         
         for (int i = 0; i < n; i++) {
