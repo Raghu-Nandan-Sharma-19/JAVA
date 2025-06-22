f0f21d71e6a378a84cc1c6278632f232e833cc5c
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-06-22 13:55:23 +0530

    Minimize max distance to gas stations better approach using priority queue

--- binarySearch/faqs/MinimizeMaxDistanceToGasStations.java
+++ binarySearch/faqs/MinimizeMaxDistanceToGasStations.java
@@ -1,37 +1,31 @@
 package binarySearch.faqs;
 
+import java.util.PriorityQueue;
 import java.util.Scanner;
 
 public class MinimizeMaxDistanceToGasStations {
     private static double minimiseMaxDistance(int[] arr, int k) {
         int n = arr.length;
+
         int[] howMany = new int[n - 1];
+        
+        PriorityQueue<double[]> pq = new PriorityQueue<>((a, b) -> Double.compare(b[0], a[0]));
 
-        for (int gasStation = 1; gasStation <= k; gasStation++) {
-            double maxSection = -1;
-            int maxIdx = -1;
-
-            for (int i = 0; i < (n - 1); i++) {
-                double difference = (arr[i + 1] - arr[i]);
-                double sectionLength = difference / (double) (howMany[i] + 1);
-
-                if (sectionLength > maxSection) {
-                    maxSection = sectionLength;
-                    maxIdx = i;
-                }
-            }
-
-            howMany[maxIdx]++;
-        }
-
-        double maxAns = -1;
         for (int i = 0; i < (n - 1); i++) {
-            double difference = (arr[i + 1] - arr[i]);
-            double sectionLength = (difference / (double) (howMany[i] + 1));
-            maxAns = Math.max(maxAns, sectionLength);
+            double distance = (arr[i + 1] - arr[i]);
+            pq.offer(new double[]{distance, i});
         }
 
-        return maxAns;
+        for (int gasStations = 1; gasStations <= k; gasStations++) {
+            double[] top = pq.poll();
+            int secondIdx = (int) (top[1]);
+            howMany[secondIdx]++;
+            double initialDistance = (arr[secondIdx + 1] - arr[secondIdx]);
+            double newSectionLength = (initialDistance / (howMany[secondIdx] + 1));
+            pq.offer(new double[]{newSectionLength, secondIdx});
+        }
+
+        return pq.peek()[0];
     }
 
     public static void main(String[] args) {
