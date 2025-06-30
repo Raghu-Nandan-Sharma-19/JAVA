37b2d508b31bb2d17713f042963bb1057046ed10
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-06-30 17:41:27 +0530

    Search in a 2d matrix better approach

--- binarySearch/twoDArrays/SearchInA2dMatrix.java
+++ binarySearch/twoDArrays/SearchInA2dMatrix.java
@@ -3,15 +3,32 @@ package binarySearch.twoDArrays;
 import java.util.Scanner;
 
 public class SearchInA2dMatrix {
+    private static boolean binarySearch(int[] arr, int target) {
+        int low = 0;
+        int high = arr.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (arr[mid] == target) {
+                return true;
+            } else if (arr[mid] > target) {
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return false;
+    }
+
     private static boolean searchMatrix(int[][] mat, int target) {
         int n = mat.length;
         int m = mat[0].length;
 
         for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (mat[i][j] == target) {
-                    return true;
-                }
+            if (mat[i][0] <= target && target <= mat[i][m - 1]) {
+                return binarySearch(mat[i], target);
             }
         }
 
