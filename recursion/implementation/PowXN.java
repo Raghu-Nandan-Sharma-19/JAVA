c1942d8172794a29d6edbd3486f49ae85d3c0831
Raghu-Nandan-Sharma-19 <raghunandansharma1901@gmail.com>
2025-07-02 13:36:09 +0530

    pow(x, n) optimal using iterative approach

--- recursion/implementation/PowXN.java
+++ recursion/implementation/PowXN.java
@@ -4,15 +4,21 @@ import java.util.Scanner;
 
 public class PowXN {
     private static double myPow(double x, int n) {
+        double ans = 1;
+
         if (n < 0) {
             x = 1 / x;
             n = -1 * n;
         }
 
-        double ans = 1;
-
-        for (int i = 1; i <= n; i++) {
-            ans *= x;
+        while (n > 0) {
+            if (n % 2 == 1) {
+                ans = ans * x;
+                n = n - 1;
+            } else {
+                x = x * x;
+                n = n / 2;
+            }
         }
 
         return ans;
